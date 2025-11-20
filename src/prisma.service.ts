406f271f241d4fa5228eba7a2057dc063f5fdfc6
hrustinszkiadam <hrustinszkiadam@gmail.com>
2025-11-20 10:17:24 +0100

    dev: updated Prisma to 7.0.0

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -1,9 +1,17 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PrismaClient } from 'generated/prisma/client';
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
-  async onModuleInit() {
-    await this.$connect();
+export class PrismaService extends PrismaClient {
+  constructor() {
+    const adapter = new PrismaMariaDb({
+      host: process.env.DATABASE_HOST || 'localhost',
+      user: process.env.DATABASE_USER || 'root',
+      password: process.env.DATABASE_PASSWORD || '',
+      database: process.env.DATABASE_NAME || 'my_database',
+      connectionLimit: 5,
+    });
+    super({ adapter });
   }
 }
